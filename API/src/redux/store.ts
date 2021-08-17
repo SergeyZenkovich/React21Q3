be6091667c7350fe6875fc3fe475e6a17cc178a8
Sergey Zenkovich <sapocalipsys.z@gmail.com>
2021-08-17 22:31:30 +0300

    refactor: move redux-devtools-extensions to dependencies

--- API/src/redux/store.ts
+++ API/src/redux/store.ts
@@ -1,7 +1,8 @@
 import {applyMiddleware, createStore} from "redux";
 import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
 import {rootReducer} from "./reducers/combinationsOfReducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
