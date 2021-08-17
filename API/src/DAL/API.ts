636ca909cf68c59ff79594173e3589d97ae87966
Sergey Zenkovich <sapocalipsys.z@gmail.com>
2021-08-18 02:28:24 +0300

    refactor:  delete unnecessary coments

--- API/src/DAL/API.ts
+++ API/src/DAL/API.ts
@@ -5,30 +5,6 @@ import {
   QueryParametersInterface,
   RequestResponseInterface
 } from "../interfaces/ComponentsInterfaces";
-// const fetchDataAsync = async (page?: number): Promise<void> => {
-//   setIsLoading(true);
-//   const result = await (
-//     await fetch(
-//       `https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${query}&page=${page || currentPage
-//       }&per_page=${elementsOnPage}&orientation=${orientation}&color=${color}&order_by=${orderBy}`
-//     )
-//   ).json();
-//   const fetchedObjects = result.results.map((el: FetchObject) => ({
-//     alt_description: el.alt_description,
-//     color: el.color,
-//     id: el.id,
-//     likes: el.likes,
-//     urls: el.urls,
-//     user: el.user
-//   }));
-//   const urls = result.results.map((el: URLs) => el.urls.regular);
-//   setFetchingElems(fetchedObjects);
-//   setTotalPages(result.total_pages);
-//   setIsLoading(false);
-// };
-
-// return [[fetchingElems, totalPages], fetchDataAsync];
-// };
 
 const API_KEY = "usJhdOuMAY8QeXFMb6GhSKgSGbOn5pH7SzDM9NPT-f0";
 
